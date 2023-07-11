a8467daaa547f5965709b47b0931eac89b88faf9
Orlundo Hubbard <weareprojectcode@gmail.com>
2023-07-11 18:43:05 -0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import * as React from 'react';
 import  Button  from '@mui/material/Button';
 import { ButtonGroup } from '@mui/material';
+import Checkbox from '@mui/material/Checkbox';
+
+const label = { inputProps: { 'aria-label': 'Checkbox demo'}};
 
 function App() {
   return (
     <div>
-      <ButtonGroup variant="contained" aria-label="outlined primary button group">
-        <Button>One</Button>
-        <Button>Two</Button>
-        <Button>Three</Button>
-      </ButtonGroup>
+      <Checkbox {...label} defaultChecked />
+<Checkbox {...label} />
+<Checkbox {...label} disabled />
+<Checkbox {...label} disabled checked />
     </div>
   );
 }
